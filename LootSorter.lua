e7e8d375f9c25c715479ee3f0303fe49471783b4
seebs <seebs@seebs.net>
2011-11-19 02:06:37 -0600

    first beta release

--- LootSorter.lua
+++ LootSorter.lua
@@ -16,24 +16,42 @@ LootSorter = ls
 ls.item_list = {}
 ls.item_count = 0
 ls.item_ordered = {}
+ls.order_asc = true
+ls.selected = nil
 
 function ls.printf(fmt, ...)
   print(string.format(fmt or 'nil', ...))
 end
 
-function ls.makeitem()
+ls.columns = {
+  { name = 'Name', key = 'name', width = 200 },
+  { name = 'Qty', key = 'qty', width = 35 },
+  { name = 'Rarity', key = 'rarity', width = 75 },
+  { name = 'Location', key = 'loc', width = 100 },
+  { name = 'Owner', key = 'owner', width = 150 },
+}
+
+function ls.makecolumns(item)
+  local xoff = 0
+  for i, v in ipairs(ls.columns) do
+    local f = item.subframes[i]
+    f:SetPoint("TOPLEFT", item.frame, "TOPLEFT", xoff, 0)
+    f:SetPoint("BOTTOMRIGHT", item.frame, "BOTTOMLEFT", xoff + v.width, 0)
+    f:SetMouseMasking("limited")
+    f.Event.LeftClick = function(...) ls.leftclick(item.index, v.key, ...) end
+    xoff = xoff + v.width + 5
+  end
+end
+
+function ls.makeitem(idx)
   local item = {}
+  item.index = idx
   item.frame = UI.CreateFrame("Frame", "Item Frame", ls.window)
-
-  item.name = UI.CreateFrame("Text", "Item Name", item.frame)
-  item.name:SetPoint("TOPLEFT", item.frame, "TOPLEFT")
-  item.name:SetPoint("BOTTOMRIGHT", item.frame, "BOTTOMLEFT", 200, 0)
-
-  item.qty = UI.CreateFrame("Text", "Item Qty", item.frame)
-  item.qty:SetPoint("TOPLEFT", item.frame, "TOPLEFT", 202, 0)
-  item.qty:SetPoint("BOTTOMRIGHT", item.frame, "BOTTOMLEFT", 250, 0)
-
-  item.frame:SetBackgroundColor(0.5, 0.5, 0.5, 1)
+  item.frame.Event.LeftClick = function(...) ls.leftclick(idx, nil, ...) end
+  item.subframes = {}
+  for i, v in ipairs(ls.columns) do
+    item.subframes[i] = UI.CreateFrame("Text", "Column", item.frame)
+  end
   return item
 end
 
@@ -42,13 +60,63 @@ function ls.setgrey(frame, count)
   frame:SetBackgroundColor(grey, grey, grey, 0.4)
 end
 
+function ls.leftclick(idx, field, ...)
+  -- try to prevent confusions
+  ls.leftup(...)
+  if idx == 0 then
+    local func = ls.order_funcs[field]
+    if func then
+      if ls.order == func then
+        ls.order_asc = not ls.order_asc
+      else
+        ls.order_asc = true
+	ls.order = func
+      end
+      ls.reorder()
+      ls.show_items()
+    end
+  elseif idx then
+    local pos = math.floor(ls.scrollbar:GetPosition())
+    ls.selected = idx + pos
+    ls.show_items()
+  end
+end
+
+-- handle dragging
+function ls.leftdown(...)
+  ls.dragging = true
+  ls.window_x = ls.window:GetLeft()
+  ls.window_y = ls.window:GetTop()
+end
+
+function ls.mousemove(...)
+  if not ls.dragging then
+    return
+  end
+  local event, x, y = ...
+  if not ls.event_x then
+    ls.event_x = x
+    ls.event_y = y
+  else
+    local newx = ls.window_x + x - ls.event_x
+    local newy = ls.window_y + y - ls.event_y
+    ls.window:SetPoint("TOPLEFT", UIParent, "TOPLEFT", newx, newy)
+  end
+end
+
+function ls.leftup(...)
+  ls.dragging = false
+  ls.event_x = nil
+  ls.event_y = nil
+end
+
 function ls.makewindow()
   if ls.window then
     ls.ui:SetVisible(true)
     ls.window:SetVisible(true)
     return
   end
-  ls.item_lines = 22
+  ls.item_lines = 20
   ls.window = UI.CreateFrame("RiftWindow", "LootSorter", ls.ui)
   ls.window:SetWidth(800)
   ls.window:SetTitle("LootSorter")
@@ -56,53 +124,208 @@ function ls.makewindow()
   local l, t, r, b = ls.window:GetTrimDimensions()
 
   ls.scrollbar = UI.CreateFrame("RiftScrollbar", "LootSorter", ls.window)
-  ls.scrollbar:SetPoint("TOPRIGHT", ls.window, "TOPRIGHT", -2 + r * -1, t + 40)
+  ls.scrollbar:SetPoint("TOPRIGHT", ls.window, "TOPRIGHT", -2 + r * -1, t + 80)
   ls.scrollbar:SetPoint("BOTTOMRIGHT", ls.window, "BOTTOMRIGHT", -2 + r * -1, -2 + b * -1)
   -- only active when there is scrolletry to do
   ls.scrollbar:SetEnabled(false)
   ls.scrollbar.Event.ScrollbarChange = ls.show_items
+  ls.window.Event.WheelBack = function() ls.scrollbar:Nudge(3) end
+  ls.window.Event.WheelForward = function() ls.scrollbar:Nudge(-3) end
   local w = ls.scrollbar:GetWidth()
 
-  ls.heading = ls.makeitem()
+  ls.window:GetContent():SetMouseMasking("full")
+  ls.window:GetBorder().Event.LeftDown = ls.leftdown
+  ls.window:GetBorder().Event.MouseMove = ls.mousemove
+  ls.window:GetBorder().Event.LeftUp = ls.leftup
+
+  ls.heading = ls.makeitem(0)
+  ls.heading.index = 0
   ls.heading.frame:SetBackgroundColor(0, 0, 0, 0)
-  ls.heading.frame:SetPoint("TOPLEFT", ls.window, "TOPLEFT", l + 2, t + 12)
-  ls.heading.frame:SetPoint("BOTTOMRIGHT", ls.window, "TOPRIGHT", -2 + (r * -1) - w, t + 32)
-  ls.show_item(ls.heading, { name = 'Name', rarity = 'Rarity', stack = 'Stack' }, true)
+  ls.heading.frame:SetPoint("TOPLEFT", ls.window, "TOPLEFT", l + 2, t + 60)
+  ls.heading.frame:SetPoint("BOTTOMRIGHT", ls.window, "TOPRIGHT", -2 + (r * -1) - w, t + 78)
+  ls.makecolumns(ls.heading)
+  ls.show_item(ls.heading, {}, true)
 
   ls.items = {}
   for i = 1, ls.item_lines do
-    ls.items[i] = ls.makeitem()
+    ls.items[i] = ls.makeitem(i)
     ls.setgrey(ls.items[i].frame, i)
-    ls.items[i].frame:SetPoint("TOPLEFT", ls.window, "TOPLEFT", l + 2, t + 20 + (20 * i))
+    ls.items[i].frame:SetPoint("TOPLEFT", ls.window, "TOPLEFT", l + 2, t + 60 + (20 * i))
     ls.items[i].frame:SetPoint("BOTTOMRIGHT", ls.window, "TOPRIGHT",
     	-2 + (r * -1) - w,
-	t + (20 * i) + 38)
+	t + (20 * i) + 78)
+    ls.makecolumns(ls.items[i])
   end
 end
 
-function ls.order(a, b)
+function ls.display_loc(frame, item)
+  local loc
+  if item._slotspec then
+    local s = lbag.slotspec_p(item._slotspec)
+    if s then
+      local type, p1, p2 = Utility.Item.Slot.Parse(item._slotspec)
+      loc = type
+    else
+      loc = "???"
+    end
+  else
+    loc = "??"
+  end
+  frame:SetText(loc)
+  frame:SetFontColor(0.98, 0.98, 0.98)
+end
+
+function ls.display_name(frame, item)
+  frame:SetText(item.name or "NO NAME")
+  local r, g, b = lbag.rarity_color(item.rarity)
+  frame:SetFontColor(r, g, b)
+end
+
+function ls.display_owner(frame, item)
+  local owner = string.match(item._charspec or "--", '.*/(.*)') or "--"
+  frame:SetText(owner)
+  if owner == '--' then
+    frame:SetFontColor(0.5, 0.5, 0.5)
+  else
+    frame:SetFontColor(0.98, 0.98, 0.98)
+  end
+end
+
+function ls.display_rarity(frame, item)
+  frame:SetText(item.rarity or "common")
+  local r, g, b = lbag.rarity_color(item.rarity)
+  frame:SetFontColor(r, g, b)
+end
+
+function ls.display_qty(frame, item)
+  local x
+  if item.stack then
+    x = string.format("%d", item.stack)
+  else
+    x = ""
+  end
+  frame:SetText(x)
+  frame:SetFontColor(0.98, 0.98, 0.98)
+end
+
+ls.display_funcs = {
+  loc = ls.display_loc,
+  qty = ls.display_qty,
+  name = ls.display_name,
+  owner = ls.display_owner,
+  rarity = ls.display_rarity,
+}
+
+function ls.order_qty(a, b)
+  local acmp, bcmp
+  a = ls.item_list[a]
+  b = ls.item_list[b]
+  acmp = a and (a.stack or 1) or 0
+  bcmp = b and (b.stack or 1) or 0
+  if acmp == bcmp then
+    return false
+  end
+  if acmp > bcmp then
+    return ls.order_asc
+  else
+    return not ls.order_asc
+  end
+end
+
+function ls.order_owner(a, b)
+  local acmp, bcmp
+  a = ls.item_list[a]
+  b = ls.item_list[b]
+  acmp = a and (a._charspec or "") or ""
+  bcmp = b and (b._charspec or "") or ""
+  if acmp == bcmp then
+    return false
+  end
+  if acmp < bcmp then
+    return ls.order_asc
+  else
+    return not ls.order_asc
+  end
+end
+
+function ls.order_name(a, b)
   local acmp, bcmp
   a = ls.item_list[a]
   b = ls.item_list[b]
   acmp = a and (a.name or "") or ""
   bcmp = b and (b.name or "") or ""
-  if acmp < bcmp then
-    return true
-  else
+  if acmp == bcmp then
     return false
+  end
+  if acmp < bcmp then
+    return ls.order_asc
+  else
+    return not ls.order_asc
   end
 end
 
-function ls.show_item(frame, item, heading)
-  frame.name:SetText(item.name or "")
-  if heading then
-    frame.name:SetFontColor(1, 1, 0.8, 1)
-    frame.qty:SetFontColor(1, 1, 0.8, 1)
-  else
-    local r, g, b = lbag.rarity_color(item.rarity)
-    frame.name:SetFontColor(r, g, b, 1)
+function ls.order_rarity(a, b)
+  local acmp, bcmp
+  a = ls.item_list[a]
+  b = ls.item_list[b]
+  acmp = a and (lbag.rarity_p(a.rarity)) or 0
+  bcmp = b and (lbag.rarity_p(b.rarity)) or 0
+  if acmp == bcmp then
+    return false
   end
-  frame.qty:SetText(tostring(item.stack or ""))
+  if acmp > bcmp then
+    return ls.order_asc
+  else
+    return not ls.order_asc
+  end
+end
+
+function ls.order_loc(a, b)
+  local acmp, bcmp
+  a = ls.item_list[a]
+  b = ls.item_list[b]
+  acmp = a and (a._slotspec) or ""
+  bcmp = b and (b._slotspec) or ""
+  if acmp == bcmp then
+    return false
+  end
+  if acmp < bcmp then
+    return ls.order_asc
+  else
+    return not ls.order_asc
+  end
+end
+
+ls.order_funcs = {
+  loc = ls.order_loc,
+  qty = ls.order_qty,
+  name = ls.order_name,
+  owner = ls.order_owner,
+  rarity = ls.order_rarity,
+}
+
+ls.order = ls.order_name
+
+function ls.show_item(frame, item, heading)
+  for i, v in ipairs(ls.columns) do
+    local sub = frame.subframes[i]
+    if not sub then
+      ls.printf("Error:  Missing subframe %d", i)
+      break
+    end
+    if heading then
+      sub:SetText(v.name)
+      sub:SetFontColor(1, 1, 0.8)
+    else
+      local func = ls.display_funcs[v.key]
+      if func then
+        func(sub, item)
+      else
+        sub:SetText(v.name)
+	sub:SetFontColor(1, 0.3, 0.3)
+      end
+    end
+  end
   frame.frame:SetVisible(true)
 end
 
@@ -115,7 +338,6 @@ function ls.show_items()
   for i = 1, ls.item_lines do
     local item_idx = i + pos
     local item_name = ls.item_ordered[item_idx]
-    ls.setgrey(ls.items[i].frame, i + pos)
     if item_name then
       local item = ls.item_list[item_name]
       if not item then
@@ -124,10 +346,19 @@ function ls.show_items()
       else
         ls.show_item(ls.items[i], item)
       end
+      if item_idx == ls.selected then
+        ls.items[i].frame:SetBackgroundColor(0.4, 0.4, 0.2, 0.4)
+      else
+        ls.setgrey(ls.items[i].frame, item_idx)
+      end
     else
       ls.items[i].frame:SetVisible(false)
     end
   end
+end
+
+function ls.reorder()
+  table.sort(ls.item_ordered, ls.order)
 end
 
 function ls.dump(filter)
@@ -138,7 +369,7 @@ function ls.dump(filter)
     ls.item_count = ls.item_count + 1
     table.insert(ls.item_ordered, k)
   end
-  table.sort(ls.item_ordered, ls.order)
+  ls.reorder()
 
   -- ensure window is available
   ls.makewindow()
@@ -186,16 +417,22 @@ function ls.slashcommand(args)
   end
   if args['C'] then
     local spec = filter:slot()
-    local slotspec, charspec = lbag.slotspec_p(spec)
-    if not slotspec then
-      slotspec = Utility.Item.Slot.All()
-    end
+    local newspec = {}
     if string.match(args['C'], '/') then
       charspec = args['C']
     else
       charspec = lbag.char_identifier(args['C'])
     end
-    filter:slot(string.format("%s:%s", charspec, slotspec))
+    for i, v in ipairs(spec) do
+      local slotspec, _ = lbag.slotspec_p(v)
+      if not slotspec then
+        table.insert(newspec, string.format("%s:%s", charspec, Utility.Item.Slot.Inventory()))
+        table.insert(newspec, string.format("%s:%s", charspec, Utility.Item.Slot.Bank()))
+      else
+        table.insert(newspec, string.format("%s:%s", charspec, slotspec))
+      end
+    end
+    filter:slot(unpack(newspec))
   end
   if args['q'] then
     if lbag.rarity_p(args['q']) then
--- ReadMe.txt
+++ ReadMe.txt
@@ -3,3 +3,9 @@ This is... well, basically, I wanted to be able to type /ls.
 It's also a sort of test addon for working on, designing, and debugging
 LibBaggotry.  Especially the "other characters' inventories" features.
 
+For now:
+	/ls -C *
+will show you everyone's inventory and bank.  Some filtering is available;
+e.g.,
+	/ls -c material -C *
+will show all things tagged as "materials".
